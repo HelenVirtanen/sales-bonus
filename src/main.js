33897bf55aaf9abaea62c23d9ab73bfa412c3cbd
Helen Virtanen <helenvirtanen28@gmail.com>
2025-07-22 14:14:19 +0300

    Add sellers and products indexes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const { ca } = require("element-plus/es/locales.mjs");
+
 /**
  * Функция для расчета выручки
  * @param purchase запись о покупке
@@ -51,7 +53,7 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    const { calculateSimpleRevenue, calculateBonusByProfit } = options;
+    const { calculateRevenue, calculateBonus } = options;
 
     // @TODO: Проверка входных данных
     if (!data ||
@@ -65,11 +67,12 @@ function analyzeSalesData(data, options) {
 
     // @TODO: Проверка наличия опций
     if (!options ||
-        typeof calculateSimpleRevenue !== 'function' ||
-        typeof calculateBonusByProfit !== 'function'
+        typeof calculateRevenue !== 'function' ||
+        typeof calculateBonus !== 'function'
     ) {
         throw new Error('Не хватает опций (функций) для работы с данными');
     } 
+
 
     // @TODO: Подготовка промежуточных данных для сбора статистики
     const sellerStats = data.sellers.map(seller => ({
@@ -81,7 +84,18 @@ function analyzeSalesData(data, options) {
         products_sold: {}
     }));
 
+    console.log(sellerStats);
+
     // @TODO: Индексация продавцов и товаров для быстрого доступа
+    const sellerIndex = Object.fromEntries(sellerStats.map(seller => [seller.id, seller]));
+    console.log('Seller indexes', sellerIndex);
+
+    const productIndex = data.products.reduce((result, product) => ({
+        ...result,
+        [product.sku]: product
+    }), {});
+
+    console.log('Product indexes', productIndex);
 
     // @TODO: Расчет выручки и прибыли для каждого продавца
 
