101d85153d0e51b76b406d85f89b7d1db528fbbe
Helen Virtanen <helenvirtanen28@gmail.com>
2025-07-22 16:33:30 +0300

    Add return of final collection of seller statistics

--- src/main.js
+++ src/main.js
@@ -137,5 +137,15 @@ function analyzeSalesData(data, options) {
         console.log('TOP PRODUCTS', seller.top_products);
     });
 
+
     // @TODO: Подготовка итоговой коллекции с нужными полями
+    return sellerStats.map(seller => ({
+        seller_id: seller.id,
+        name: seller.name, 
+        revenue: +seller.revenue.toFixed(2),
+        profit: +seller.profit.toFixed(2),
+        sales_count: +seller.sales_count,
+        top_products: seller.top_products,
+        bonus: +seller.bonus.toFixed(2)
+    })); 
 }
